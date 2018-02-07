5947e9897e3ef370df7cbb2c727adee91542bce2
every1no1 <36211516+every1no1@users.noreply.github.com>
2018-02-07 01:37:50 +0100

    Update RpcInvalidResponseException.java

--- src/RPCWrapper/Exception/RpcInvalidResponseException.java
+++ src/RPCWrapper/Exception/RpcInvalidResponseException.java
@@ -1,8 +1,5 @@
 package RPCWrapper.Exception;
 
-/**
- * Created by CIA on 29/08/2017.
- */
 public class RpcInvalidResponseException extends RpcException {
 
     public RpcInvalidResponseException(String message) {
