20465d924ff2f4eea72b3703d7ff63333f18efbb
every1no1 <36211516+every1no1@users.noreply.github.com>
2018-02-07 01:36:59 +0100

    Update InsufficientFundsException.java

--- src/RPCWrapper/Exception/InsufficientFundsException.java
+++ src/RPCWrapper/Exception/InsufficientFundsException.java
@@ -1,8 +1,5 @@
 package RPCWrapper.Exception;
 
-/**
- * Created by CIA on 29/08/2017.
- */
 public class InsufficientFundsException extends RpcException {
 
     public InsufficientFundsException(String message) {
